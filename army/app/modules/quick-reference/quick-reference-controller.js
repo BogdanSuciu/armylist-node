48de0e57de694f8640360b84a37cc59a8b745ea0
Suciu Bogdan <bogdan.suciu@gmail.com>
2017-02-19 19:06:16 +0200

    integrated get weapon skill table into quick reference controller

--- army/app/modules/quick-reference/quick-reference-controller.js
+++ army/app/modules/quick-reference/quick-reference-controller.js
@@ -7,44 +7,72 @@
  */
 (function () {
   "use strict";
-  angular.module("armyApp").controller("quickReference", function ($scope, scopeService) {
+  angular.module("armyApp").controller("quickReference", function ($scope, scopeService, requestServices) {
     if (!scopeService.shared.quickReference) {
       scopeService.shared.quickReference = {};
     }
     $scope.quickReference = scopeService.shared.quickReference;
     let _this = $scope.quickReference;
 
-    // TODO Use server provided data
-    _this.ballisticTable = {1: "6", 2: "5", 3: "4", 4: "3", 5: "2", 6: "2/6", 7: "2/5", 8: "2/4", 9: "2/3", 10: "2/2"};
-
-    _this.weaponSkillTable = {
-      1: ["4+", "4+", "5+", "5+", "5+", "5+", "5+", "5+", "5+", "5+"],
-      2: ["3+", "4+", "4+", "5+", "5+", "5+", "5+", "5+", "5+", "5+"],
-      3: ["3+", "3+", "4+", "4+", "4+", "5+", "5+", "5+", "5+", "5+"],
-      4: ["3+", "3+", "3+", "4+", "4+", "4+", "4+", "4+", "5+", "5+"],
-      5: ["3+", "3+", "3+", "3+", "4+", "4+", "4+", "4+", "4+", "4+"],
-      6: ["3+", "3+", "3+", "3+", "3+", "4+", "4+", "4+", "4+", "4+"],
-      7: ["3+", "3+", "3+", "3+", "3+", "3+", "4+", "4+", "4+", "4+"],
-      8: ["3+", "3+", "3+", "3+", "3+", "3+", "3+", "4+", "4+", "4+"],
-      9: ["3+", "3+", "3+", "3+", "3+", "3+", "3+", "3+", "4+", "4+"],
-      10: ["3+", "3+", "3+", "3+", "3+", "3+", "3+", "3+", "3+", "4+"]
-    };
-
-    _this.ToWoundTable = {
-      1: ["4+", "5+", "6+", "6+", "-", "-", "-", "-", "-", "-"],
-      2: ["3+", "4+", "5+", "6+", "6+", "-", "-", "-", "-", "-"],
-      3: ["2+", "3+", "4+", "5+", "6+", "6+", "-", "-", "-", "-"],
-      4: ["2+", "2+", "3+", "4+", "5+", "6+", "6+", "-", "-", "-"],
-      5: ["2+", "2+", "2+", "3+", "4+", "5+", "6+", "6+", "-", "-"],
-      6: ["2+", "2+", "2+", "2+", "3+", "4+", "5+", "6+", "6+", "-"],
-      7: ["2+", "2+", "2+", "2+", "2+", "3+", "4+", "5+", "6+", "6+"],
-      8: ["2+", "2+", "2+", "2+", "2+", "2+", "3+", "4+", "5+", "6+"],
-      9: ["2+", "2+", "2+", "2+", "2+", "2+", "2+", "3+", "4+", "5+"],
-      10: ["2+", "2+", "2+", "2+", "2+", "2+", "2+", "2+", "3+", "4+"]
-    };
+    _this.weaponSkillTable = {};
 
     if (!_this.visibleTable) {
       _this.visibleTable = "ballistic";
+    }
+
+    _this.referenceTables = {
+      "melee": {
+        values: false,
+        getValues: function() {
+          return requestServices.getWeaponSkillTable().then(function(response) {
+            return response.values;
+          }).catch(function(error){
+            console.error("error")
+            return false;
+          });
+        }
+      },
+      "ballistic": {
+        values: {1: "6", 2: "5", 3: "4", 4: "3", 5: "2", 6: "2/6", 7: "2/5", 8: "2/4", 9: "2/3", 10: "2/2"},
+        getValues: function() {
+          return true;
+        }
+      },
+      "wound": {
+        values: {
+          1: ["4+", "5+", "6+", "6+", "-", "-", "-", "-", "-", "-"],
+          2: ["3+", "4+", "5+", "6+", "6+", "-", "-", "-", "-", "-"],
+          3: ["2+", "3+", "4+", "5+", "6+", "6+", "-", "-", "-", "-"],
+          4: ["2+", "2+", "3+", "4+", "5+", "6+", "6+", "-", "-", "-"],
+          5: ["2+", "2+", "2+", "3+", "4+", "5+", "6+", "6+", "-", "-"],
+          6: ["2+", "2+", "2+", "2+", "3+", "4+", "5+", "6+", "6+", "-"],
+          7: ["2+", "2+", "2+", "2+", "2+", "3+", "4+", "5+", "6+", "6+"],
+          8: ["2+", "2+", "2+", "2+", "2+", "2+", "3+", "4+", "5+", "6+"],
+          9: ["2+", "2+", "2+", "2+", "2+", "2+", "2+", "3+", "4+", "5+"],
+          10: ["2+", "2+", "2+", "2+", "2+", "2+", "2+", "2+", "3+", "4+"]
+        },
+        getValues: function() {
+          return true;
+        }
+      }
+    };
+
+    _this.toggleReferenceTable = function(table) {
+
+      if(_this.referenceTables[table]) {
+
+        _this.visibleTable = table;
+
+        if(!_this.referenceTables[table].values) {
+          _this.referenceTables[table].getValues().then(function(response){
+            _this.referenceTables[table].values = response;
+          }).catch(function(error){
+            console.error(error);
+          });
+        }
+
+      }
+
     }
 
   });
