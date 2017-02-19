7cb174d2ed83feca442195094306b2c09144c44e
Suciu Bogdan <bogdan.suciu@gmail.com>
2017-02-19 17:37:31 +0200

    code formating

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,62 +16,62 @@ var cssList = [
 var fileObject = {};
 
 // constructing the file object
-for(var i=0; i<cssList.length; i++) {
+for (var i = 0; i < cssList.length; i++) {
   var fileName = cssList[i];
-  fileObject[fileName + ".css"] =  [fileName + ".less"];
+  fileObject[fileName + ".css"] = [fileName + ".less"];
 }
 
 // exporting grunt task
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   require('jit-grunt')(grunt);
 
   grunt.initConfig({
     webpack: {
-        options: {
-          context: __dirname + '\\army',
-          entry: {
-            app: '.\\index.js',
-            vendor: [
-              'angular',
-              'ngstorage-webpack',
-              'angular-route',
-              'jquery',
-              'bootstrap'
-            ]
-          },
-          output: {
-            path: __dirname + '\\army\\dist',
-            filename: 'bundle.js'
-          },
-          module: {
-            loaders: [
-              {
-                test: /\.js/,
-                loader: 'babel',
-                include: __dirname + '\\army',
-               },
-              {
-                  test: /\.less$/,
-                  loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
-              }
-            ],
-          },
-          plugins: [
-          
-            new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js"),
-            
-            new webpack.ProvidePlugin({
-              $: "jquery",
-              jQuery: "jquery"
-            }),
-            
-            new ExtractTextPlugin("styles.css")
-            
+      options: {
+        context: __dirname + '\\army',
+        entry: {
+          app: '.\\index.js',
+          vendor: [
+            'angular',
+            'ngstorage-webpack',
+            'angular-route',
+            'jquery',
+            'bootstrap'
           ]
         },
-        build: {
-            // configuration for this build
-        }
+        output: {
+          path: __dirname + '\\army\\dist',
+          filename: 'bundle.js'
+        },
+        module: {
+          loaders: [
+            {
+              test: /\.js/,
+              loader: 'babel',
+              include: __dirname + '\\army',
+            },
+            {
+              test: /\.less$/,
+              loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+            }
+          ],
+        },
+        plugins: [
+
+          new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js"),
+
+          new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+          }),
+
+          new ExtractTextPlugin("styles.css")
+
+        ]
+      },
+      build: {
+        // configuration for this build
+      }
     },
     watch: {
       webpack: {
